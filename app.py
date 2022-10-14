8fb2832b331c8269bf3688f155a2e646244da1b4
kcin999 <62564441+kcin999@users.noreply.github.com>
2022-10-13 20:03:49 -0400

    Messed around with some file picker stuff

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from PyQt5.QtWidgets import *
 import sys
+from PyQt5.QtWidgets import QGroupBox, QSpinBox, QComboBox, QDialogButtonBox, QVBoxLayout, QFormLayout, QPushButton, QLineEdit, QFileDialog, QApplication, QDialog
 
 # creating a class
 # that inherits the QDialog class
 
 
 class Window(QDialog):
-
-    file_1 = "File 1"
-    file_2 = "File 2"
-    file_3 = "File 3"
 
     # constructor
     def __init__(self):
@@ -21,84 +17,79 @@ class Window(QDialog):
         # setting geometry to the window
         self.setGeometry(100, 100, 300, 400)
 
-        # creating a group box
-        self.file_picker_group = QGroupBox("File Picker")
-
-        # creating spin box to select age
-        self.ageSpinBar = QSpinBox()
-
-        # creating combo box to select degree
-        self.degreeComboBox = QComboBox()
-
-        # adding items to the combo box
-        self.degreeComboBox.addItems(["BTech", "MTech", "PhD"])
-
-        # calling the method that create the form
-        self.create_file_picker_form()
-
         # creating a dialog button for ok and cancel
-        self.buttonBox = QDialogButtonBox(
+        self.button_box = QDialogButtonBox(
             QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
 
         # adding action when form is accepted
-        self.buttonBox.accepted.connect(self.getInfo)
+        self.button_box.accepted.connect(self.close)
 
         # adding action when form is rejected
-        self.buttonBox.rejected.connect(self.reject)
+        self.button_box.rejected.connect(self.reject)
 
         # creating a vertical layout
-        mainLayout = QVBoxLayout()
+        main_layout = QVBoxLayout()
 
         # adding form group box to the layout
-        mainLayout.addWidget(self.file_picker_group)
+        main_layout.addWidget(self.create_file_picker_form())
 
         # adding button box to the layout
-        mainLayout.addWidget(self.buttonBox)
+        main_layout.addWidget(self.button_box)
 
         # setting lay out
-        self.setLayout(mainLayout)
+        self.setLayout(main_layout)
 
-    # get info method called when form is accepted
-    def getInfo(self):
-
-        # printing the form information
-        print("Person Name : {0}".format(self.nameLineEdit.text()))
-        print("Degree : {0}".format(self.degreeComboBox.currentText()))
-        print("Age : {0}".format(self.ageSpinBar.text()))
-
-        # closing the window
-        self.close()
-
-    # creat form method
     def create_file_picker_form(self):
+        """Creates and retuns the Box for the File Picker
+        """
+        # Creates one QGroupBox
+        file_picker_group = QGroupBox("File Picker")
 
         # creating a form layout
         layout = QFormLayout()
 
-        #Create Puttons
-        file_button_1 = QPushButton(self.file_1)
-        file_button_1.clicked.connect(self.file_picker)
-        file_button_2 = QPushButton(self.file_2)
-        file_button_2.clicked.connect(self.file_picker)
-        file_button_3 = QPushButton(self.file_3)
-        file_button_3.clicked.connect(self.file_picker)
+        # Create Puttons
+        file_button_1 = QPushButton("Choose File")
+        self.file1_path = QLineEdit()
+        file_button_1.clicked.connect(
+            lambda: self.file_picker(self.file1_path)
+        )
+
+        file_button_2 = QPushButton("Choose File")
+        self.file2_path = QLineEdit()
+        file_button_2.clicked.connect(
+            lambda: self.file_picker(self.file2_path)
+        )
+
+        file_button_3 = QPushButton("Choose File")
+        self.file3_path = QLineEdit()
+        file_button_3.clicked.connect(
+            lambda: self.file_picker(self.file3_path)
+        )
 
         # adding rows
-        layout.addRow(file_button_1)
+        layout.addRow(self.file1_path, file_button_1)
 
-        layout.addRow(file_button_2)
+        layout.addRow(self.file2_path, file_button_2)
 
-        layout.addRow(file_button_3)
+        layout.addRow(self.file3_path, file_button_3)
 
         # setting layout
-        self.file_picker_group.setLayout(layout)
+        file_picker_group.setLayout(layout)
 
-    def file_picker(self):
+        return file_picker_group
+
+    def file_picker(self, q_line_edit: QLineEdit = None):
+        """Prompts the user for a file input, and
+        sets the QLineEdit's text to the file, if it is not none"""
         file, check = QFileDialog.getOpenFileName(None, "Choose a file",
-                                                "", "CSV File (*.csv)")
+                                                  "", "CSV File (*.csv)")
         if check:
             print(file)
-            return file
+
+            if q_line_edit is not None:
+                q_line_edit.setText(file)
+
 
 # main method
 if __name__ == '__main__':
